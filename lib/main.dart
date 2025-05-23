e8dfee88fe19bdfb7efa6d97566fc7d4fdbe1822
gregoryo57 <gregoryowensjunior@gmail.com>
2025-05-23 02:04:40 -0400

    added flutterfire and fixed google plugin version issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:proj/firebase_options.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
 
   runApp(const MyApp());
 }
 void pr(){
-  print('\nbellooooo'*2);
+  print('\nnigga'*2);
 }
 
 class MyApp extends StatelessWidget {
