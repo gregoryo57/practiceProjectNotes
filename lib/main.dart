3363d7ee97d2d81f1ac9b02af3a8bc5ee6a6ba44
gregoryo57 <gregoryowensjunior@gmail.com>
2025-06-01 00:24:58 -0400

    full firebase initialization and switch cases for login

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proj/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -42,33 +44,52 @@ class _HomePageState extends State<HomePage>{
       appBar: AppBar(
           title: const Text('Register'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            decoration: const InputDecoration(
-              hintText: 'Enter your email here'
-            ),
-          ),
-          TextField(
-            controller: _password,
-            decoration: const InputDecoration(
-                hintText: 'Enter your password here'
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: email,
-                  password: password,
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+              return Column(
+                  children:
+                  [
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                          hintText: 'Enter your email here'
+                      ),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                          hintText: 'Enter your password here'
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        final userCredential =
+                        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        print(userCredential);
+                      },
+                      child: Text('Register'),
+                    ),
+                  ]
               );
-              print (userCredential);
-            },
-            child: Text('Register'),
-          ),
-        ]
+            default:
+              return const Text('Loading...');
+          }
+        }
       ),
     );
   }
