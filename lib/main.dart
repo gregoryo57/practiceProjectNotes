056e3aaf1393286e16e5c4b3168224d9ae932659
gregoryo57 <gregoryowensjunior@gmail.com>
2025-06-02 20:03:33 -0400

    Added two seperate view files for login and register, as well as try/catch statements for login and register errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:proj/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
-
+import 'package:proj/views/login_view.dart';
+import 'package:proj/views/register_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -14,83 +11,18 @@ void main() {
     home: const HomePage(),
   ));
 }
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-class _HomePageState extends State<HomePage>{
-
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text('Register'),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState){
-            case ConnectionState.done:
-              return Column(
-                  children:
-                  [
-                    TextField(
-                      controller: _email,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                          hintText: 'Enter your email here'
-                      ),
-                    ),
-                    TextField(
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                          hintText: 'Enter your password here'
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-                        final userCredential =
-                        await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        print(userCredential);
-                      },
-                      child: Text('Register'),
-                    ),
-                  ]
-              );
-            default:
-              return const Text('Loading...');
-          }
-        }
+      appBar: AppBar(title: const Text('Home Page'),
       ),
     );
+
   }
 }
+
+
